3a1c1949e0f98dd62c791c7d47bcef219ea0daf4
Preston Jennings <prestonj@gmail.com>
2015-06-08 20:00:48 -0400

    First cut of the rackafx one pole LPF project

---
+++ rackafx/SinglePoleFilter/RackAFXDLL.cpp
@@ -0,0 +1,75 @@
+#include "RackAFXDLL.h"
+//#define RAFX_DLLMAIN
+
+// WARNING: DO NOT UNDER ANY CIRCUMSTANCES EDIT THIS FILE! **********************
+
+///////////////////////////////////////////////////////////
+//
+// Global variables
+//
+void* g_hModule;   // DLL module handle
+#define stringCompanyName "RackAFX User"
+static float g_fMUResult;
+unsigned long uPCID1 = 4294967208;
+unsigned long uPCID2 = 4294967202;
+unsigned long uPCID3 = 4294967221;
+unsigned long uPCID4 = 4292215654;
+unsigned long uCCID1 = 4294945913;
+unsigned long uCCID2 = 1298219125;
+unsigned long uCCID3 = 4290391044;
+unsigned long uCCID4 = 4294935561;
+// --- VSTCompatibility: ENABLED --- //
+
+
+///////////////////////////////////////////////////////////
+//
+// DLL module information
+//
+
+extern "C"
+{
+	// entry point DllMain
+	BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD dwReason, LPVOID /*lpvReserved*/)
+	{
+		if(dwReason == DLL_PROCESS_ATTACH)
+		{
+			g_hModule = hInst;
+
+			// --- VST2 and VST3 -- DO NOT EDIT
+			CRAFXPlugInContainer::createRAFXPlugInContainer(g_hModule, createObject, uPCID1, uPCID2, uPCID3, uPCID4, uCCID1, uCCID2, uCCID3, uCCID4, &g_pGUIXML[0], g_nGUIXMLSize, stringCompanyName);
+		}
+		else if(dwReason == DLL_PROCESS_DETACH)
+		{
+			// --- VST2 and VST3 -- DO NOT EDIT
+			CRAFXPlugInContainer::destroyRAFXPlugInContainer();
+		}
+
+		return TRUE ;
+	}
+	// end entry point DllMain*/
+
+	// --- VST3 ONLY: not used now but I may use later
+	/*bool DllExportVST3 InitDll()
+	{
+		return true;
+	}
+	bool DllExportVST3 ExitDll()
+	{
+		return true;
+	}*/
+}
+
+//RackAFX Creation Function
+DllExport CPlugIn* createObject()
+{
+	CPlugIn* pOb = new CSinglePoleFilter(); // ***
+	return pOb;
+}
+
+DllExport UINT getAPIVersion()
+{
+	return CURRENT_PLUGIN_API;
+}
+
+
+
