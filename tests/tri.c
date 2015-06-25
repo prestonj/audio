06e3a078c64675ef6b78e26f9c1a9ceb46a952fb
Preston Jennings <prestonj@gmail.com>
2015-06-25 15:57:37 -0400

    tri fns

---
+++ tests/tri.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float pwmfreq = 78125;
+float slice = 1 / pwmfreq;
+float triFreq = 1000;
+float triPeriod = pwmfreq / triFreq;
+float triAmp = 1;
+float accum = 0;
+
+int halfAmp = 512;
+int fullAmp = 1024;
+
+//square(t) = sgn(sin(2πt))
+//sawtooth(t) = t - floor(t + 1/2)
+//triangle(t) = abs(sawtooth(t))
+
+float sawtooth(float t)
+{
+	return t - floor(t + (t/2));
+}
+
+int triAsInt()
+{
+	accum += 1; // assumption - each call is +1 freq
+	if (accum > triFreq)
+	{
+		accum -= triFreq;
+	}
+	float x = triAmp * ((accum*2 - triPeriod) / triPeriod);
+	return halfAmp + (halfAmp * x);
+}
+
+int main()
+{
+
+	for (int i = 0; i < (int) triPeriod; ++i)
+	{
+	}
+	return 0;
+}
